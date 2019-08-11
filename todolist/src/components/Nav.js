30286af17fe3077e478837c11f66f17f8c271e5e
mdy6099 <mdy6099@naver.com>
2019-08-11 23:58:46 +0900

    made NAV library and applied this to NAV

--- todolist/src/components/Nav.js
+++ todolist/src/components/Nav.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import CategoryLabel from "./CategoryLabel"
 import CategoryAdd from "./CategoryAdd"
+import {NAV_getLastPage, NAV_getCurrentPage, NAV_getLastBoundaryIndex, NAV_getStartIndexAfterAdd, NAV_getStartIndexAfterDelete, NAV_getStartIndexOfPrev, NAV_getStartIndexOfNext} from '../library/NAV_library' 
 
 class Nav extends Component {
     constructor(props) {
@@ -20,23 +21,25 @@ class Nav extends Component {
     getLastPage() {
         const numOFCategories = this.props.categoryList.length
         const allowedNumPerPage = this.maxNumOfCategoryPerPage
-        return Math.floor((numOFCategories-1)/allowedNumPerPage) 
+        return NAV_getLastPage(numOFCategories, allowedNumPerPage)
     }
 
     renderCategories() {//보여줄 카테고리의 object list를 array로 반환합니다.
         const startIndex = this.state.categoryStartIndex
         const numOFCategories = this.props.categoryList.length
         const allowedNumPerPage = this.maxNumOfCategoryPerPage
-        const lastBound = (numOFCategories > (startIndex + allowedNumPerPage) ? (startIndex + allowedNumPerPage) : numOFCategories)//어떤 오브젝트들을 보내줄지 선택
+        const lastBound = NAV_getLastBoundaryIndex(numOFCategories, allowedNumPerPage, startIndex)
         return this.props.categoryList.slice(startIndex, lastBound)
     }
 
     handleCategoryDelete(categoryId) {//카테고리를 지워서 마지막 페이지가 비는 경우를 handle
         const superDelete = this.props.handleCategoryDelete;
-        const {categoryStartIndex} = this.state
+        const startIndex = this.state.categoryStartIndex
         const numOFCategories = this.props.categoryList.length
-        if(numOFCategories === categoryStartIndex + 1) {//ex 9개에서 1개 지우는데 한 페이지 표시가능 갯수가 8개인 경우 + 현재가 마지막 페이지인 경우
-            this.setState({categoryStartIndex: categoryStartIndex-this.maxNumOfCategoryPerPage})//이전페이지로 돌아간다
+        const allowedNumPerPage = this.maxNumOfCategoryPerPage
+        const startIndexAfterDelete = NAV_getStartIndexAfterDelete(numOFCategories, allowedNumPerPage, startIndex)
+        if(startIndex !== startIndexAfterDelete) {//ex 9개에서 1개 지우는데 한 페이지 표시가능 갯수가 8개인 경우 + 현재가 마지막 페이지인 경우
+            this.setState({categoryStartIndex: startIndexAfterDelete})//이전페이지로 돌아간다
         }
         superDelete(categoryId)//원래 지우는 기능
     }
@@ -46,29 +49,29 @@ class Nav extends Component {
         const numOFCategories = this.props.categoryList.length
         const allowedNumPerPage = this.maxNumOfCategoryPerPage
         const startIndex = this.state.categoryStartIndex
-        const lastPageIndex = allowedNumPerPage*(Math.floor((numOFCategories-1)/allowedNumPerPage))
-        if(numOFCategories!==0 && numOFCategories % allowedNumPerPage === 0) {//ex 현재 8*n개인데 한페이지 표시가능 갯수가 8개라서 하나 더하면 새로운 페이지에 나오는 경우
-            this.setState({categoryStartIndex: numOFCategories})// 새로운 페이지로 간다.
-        }
-        else if(startIndex!==lastPageIndex) {//마지막 페이지가 아니였으면 마지막 페이지로 간다.
-            this.setState({categoryStartIndex: lastPageIndex})
+        const startIndexAfterAdd = NAV_getStartIndexAfterAdd(numOFCategories, allowedNumPerPage)
+        if(startIndex !== startIndexAfterAdd ) {//ex 현재 8*n개인데 한페이지 표시가능 갯수가 8개라서 하나 더하면 새로운 페이지에 나오는 경우
+            this.setState({categoryStartIndex: startIndexAfterAdd})// 새로운 페이지로 간다.
         }
         superAdd(newCategoryName, newcategoryId)//원래 더하는 기능
     }
 
     handleCategoryPrev() {
-        const {categoryStartIndex} = this.state
-        if(categoryStartIndex>0) {//이전 페이지가 존재하면
-            this.setState({categoryStartIndex: categoryStartIndex-this.maxNumOfCategoryPerPage})//이전 페이지로 돌아간다.
+        const startIndex = this.state.categoryStartIndex
+        const allowedNumPerPage = this.maxNumOfCategoryPerPage
+        const startIndexAfterPrev = NAV_getStartIndexOfPrev(allowedNumPerPage,startIndex)
+        if(startIndex!==startIndexAfterPrev) {//이전 페이지가 존재하면
+            this.setState({categoryStartIndex: startIndexAfterPrev})//이전 페이지로 돌아간다.
         }
     }
 
     handleCategoryNext() {
-        const {categoryStartIndex} = this.state
+        const startIndex = this.state.categoryStartIndex
         const numOFCategories = this.props.categoryList.length
-        const nextCategoryStartIndex = categoryStartIndex + this.maxNumOfCategoryPerPage
-        if(nextCategoryStartIndex<numOFCategories) {//다음 페이지에 표시할 게 있다면
-            this.setState({categoryStartIndex: nextCategoryStartIndex})//다음 페이지로 넘어간다.
+        const allowedNumPerPage = this.maxNumOfCategoryPerPage
+        const startIndexAfterNext = NAV_getStartIndexOfNext(numOFCategories, allowedNumPerPage, startIndex)
+        if(startIndex !== startIndexAfterNext) {//다음 페이지에 표시할 게 있다면
+            this.setState({categoryStartIndex: startIndexAfterNext})//다음 페이지로 넘어간다.
         }
     }
 
@@ -92,7 +95,7 @@ class Nav extends Component {
                 />
                 <div>
                     <button onClick={this.handleCategoryPrev} >{`prev`}</button>
-                    {`${nav.state.categoryStartIndex / nav.maxNumOfCategoryPerPage}번째 페이지 / ${this.getLastPage()}`}
+                    {`${NAV_getCurrentPage(nav.maxNumOfCategoryPerPage, nav.state.categoryStartIndex)}번째 페이지 / ${this.getLastPage()}`}
                     <button onClick={this.handleCategoryNext} >{`next`}</button>
                 </div>
             </div>
